442aafeee2d8280401f265aaaacc626cd0c7a1f5
guastileticia <guastile@gmail.com>
2026-01-29 01:22:46 +0000

    segundo commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,12 @@
 import streamlit as st
 
-st.title("Fica aqui entre nós.")
+st.image("img/corte.png")
+
+st.title("Fica aqui entre nós.🫂 ")
+st.markdown("""---""")
 st.header("Um espaço de escuta segura que alivia a angústia.", divider=True)
-st.write(
-    
-)
+st.markdown(">Espaço seguro.")
+st.markdown(">:heart:")
+
+
+
